265f6a568fdae891b9ed8db425ae9defbf9aae12
Alexander D. Macri <admfav2012@gmail.com>
2014-03-06 13:29:56 -0500

    Create TIcTockToe.c

---
+++ TIcTockToe.c
@@ -0,0 +1,38 @@
+// Created by ALexander Macri
+//
+//
+
+#include <stdio.h>
+
+typedef struct{
+  char xo;
+}BoardInfo;
+
+typedef struct{
+  int* position;
+  char xo;
+}PlayerInfo;
+
+typedef struct{
+  int x;
+  int y;
+}pos;
+
+
+void readInput(PlayerInfo player){
+  char in;
+  do{
+    
+  }while(
+}
+
+
+int main(int argc,const char** argv){
+  
+  BoardInfo* board[3][3];
+  PlayerInfo p1;
+  PlayerInfo p2;
+  
+  
+  
+};
